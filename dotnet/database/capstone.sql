e2e71d6a963e245c6b62a249df002b02e4a27722
Tiana Silva <tianarenees@gmail.com>
2024-04-06 18:32:11 -0400

    Saved db

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -16,17 +16,31 @@ GO
 
 --create tables
 CREATE TABLE users (
-	user_id int IDENTITY(1,1) NOT NULL,
-	username varchar(50) NOT NULL,
+	id int IDENTITY(1,1) NOT NULL,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	email varchar(80) NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	salt varchar(200) NOT NULL,
 	user_role varchar(50) NOT NULL
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
+	CONSTRAINT PK_user PRIMARY KEY (id)
+)
+
+CREATE TABLE temp_users (
+	temp_id int IDENTITY(1,1) NOT NULL,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	email varchar(80) NOT NULL,
+	password_hash varchar(200) NOT NULL,
+	salt varchar(200) NOT NULL,
+	user_role varchar(50) NOT NULL,
+	confirm_code int NOT NULL,
+	CONSTRAINT PK_temp_users PRIMARY KEY (temp_id)
 )
 
 -- populate default data
 -- password for these is "password"
-INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
-INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+INSERT INTO users (first_name, last_name, email, password_hash, salt, user_role) VALUES ('User', 'Test','user@storage.com','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
+INSERT INTO users (first_name, last_name, email, password_hash, salt, user_role) VALUES ('Admin', 'Test','admin@storage.com','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
 
 GO
