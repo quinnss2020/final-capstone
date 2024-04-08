04483322af1f5c24c1d9cba23d0a395ac55dd315
Serina Alayed <alayed.serina@gmail.com>
2024-04-08 17:06:41 -0400

    vue/src/

---
+++ vue/src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="nav">
+        <section>
+            <router-link v-bind:to="{name: 'home'}"> HOME </router-link> 
+            <router-link v-bind:to="{name: 'listedUnits'}"> LISTED UNITS </router-link> 
+            <router-link v-bind:to="{name: 'login'}"> LOGIN </router-link> 
+            <router-link v-bind:to="{name: 'register'}"> REGISTER </router-link>
+
+        </section>
+        
+
+    </section>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
