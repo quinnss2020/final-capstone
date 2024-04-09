5e2b2ebb552694116ece46ab5f831bfd63405016
Serina Alayed <alayed.serina@gmail.com>
2024-04-09 12:54:45 -0400

    capstone

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="capstone-app">
-    <div id="nav">
+    <navigation/>
+    <!-- <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="this.$store.state.token != ''">Logout</router-link>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
 
+<script>
+import Navigation from "./components/Navigation.vue"
+
+export default {
+  name: 'App',
+  components: {Navigation},
+
+}
+
+</script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
--- vue/src/components/Navigation.vue
+++ vue/src/components/Navigation.vue
@@ -1,19 +1,50 @@
 <template>
     <section class="nav">
-        <section>
-            <router-link v-bind:to="{name: 'home'}"> HOME </router-link> 
-            <router-link v-bind:to="{name: 'listedUnits'}"> LISTED UNITS </router-link> 
+        <ol id="nav-buttons">
+            <li><a href="#">LISTED UNITS</a></li>
+           <li><router-link v-bind:to="{name: 'login'}"> LOGIN </router-link></li>
+           <li><a href="#">REGISTER</a></li>
+           <li><a href="#">LOGOUT</a></li>
+            <!-- <router-link v-bind:to="{name: 'listedUnits'}"> LISTED UNITS </router-link> 
             <router-link v-bind:to="{name: 'login'}"> LOGIN </router-link> 
-            <router-link v-bind:to="{name: 'register'}"> REGISTER </router-link>
-
-        </section>
-        
-
+            <router-link v-bind:to="{name: 'register'}"> REGISTER </router-link> -->
+            <li><router-link v-bind:to="{ name: 'logout' }" v-if="this.$store.state.token != ''">LOGOUT</router-link></li>
+            </ol>
     </section>
 </template>
 
 <script>
 </script>
 
-<style>
+<style scoped>
+
+nav> :is(ul,ol) {
+    list-style: none;
+   
+}
+#nav-buttons {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+
+    margin: 0;
+    list-style: none;
+    background: none;
+}
+
+a {
+    display: flex;
+    text-decoration: none;
+    background-color: #264B56;
+    color: #faefe0;
+    font-size: 1.2rem;
+    font-weight: 500;
+    padding: 2rem;
+    border-style: outset;
+    border-color: #264B56;
+    border-radius: 2rem;
+    padding-inline: 1.5rem;
+    padding-block: 0.6rem;
+}
+
 </style>
