191853cdef928822fcb77247d8937a10b51e692a
Tiana Silva <tianarenees@gmail.com>
2024-04-10 15:41:50 -0400

    pulling server

---
+++ vue/src/components/Unit.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="unit-card">
+        <p>City: </p>
+        <p>#{{  }}</p>
+        <p>Size: </p>
+
+        
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped></style>
