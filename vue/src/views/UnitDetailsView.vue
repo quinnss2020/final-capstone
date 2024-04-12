e0c6de8d1b2863d111beb3709a3086406ac622fa
Serina Alayed <alayed.serina@gmail.com>
2024-04-12 12:41:47 -0400

    m

---
+++ vue/src/views/UnitDetailsView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="unit-details">
+        <h1>{{ item.city }} {{ item.id }}</h1>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'unit',
+    props: ['item']
+}
+
+</script>
+
+<style>
+</style>
