1884cfaa684cf0baddde13a41608f8ece8887a54
Serina Alayed <alayed.serina@gmail.com>
2024-04-14 19:47:03 -0400

    styling to login/register, buttons, background

--- vue/src/views/RegisterView.vue
+++ vue/src/views/RegisterView.vue
@@ -1,35 +1,41 @@
 <template>
-  <div id="register" class="text-center">
-    <form v-on:submit.prevent="register">
-      <h1>Create Account</h1>
-      <div role="alert" v-if="registrationErrors">
-        {{ registrationErrorMsg }}
+  <div id="container">
+    <div id="register" class="text-center">
+      <form v-on:submit.prevent="register">
+        <h1>Create Account</h1>
         <br>
+        <div role="alert" v-if="registrationErrors">
+          {{ registrationErrorMsg }}
+          <br>
+          <br>
+        </div>
+        <div class="form-input-group">
+          <label for="first-name"></label>
+          <input type="text" id="firstname" placeholder="First Name" v-model="user.firstname" required autofocus />
+        </div>
+        <div class="form-input-group">
+          <label for="last-name"></label>
+          <input type="text" id="lastname" placeholder="Last Name" v-model="user.lastname" required autofocus />
+        </div>
+        <div class="form-input-group">
+          <label for="email"></label>
+          <input type="text" id="email" placeholder="Email" v-model="user.email" required autofocus />
+        </div>
+        <div class="form-input-group">
+          <label for="password"></label>
+          <input type="password" placeholder="Password" id="password" v-model="user.password" required />
+        </div>
+        <div class="form-input-group">
+          <label for="confirmPassword"></label>
+          <input type="password" placeholder="Confirm Password" id="confirmPassword" v-model="user.confirmPassword"
+            required /> 
+          </div>
+          <br>
+          <button type="submit">CREATE ACCOUNT</button>
         <br>
-      </div>
-      <div class="form-input-group">
-        <label for="first-name">First Name</label>
-        <input type="text" id="firstname" v-model="user.firstname" required autofocus />
-      </div>
-      <div class="form-input-group">
-        <label for="last-name">Last Name</label>
-        <input type="text" id="lastname" v-model="user.lastname" required autofocus />
-      </div>
-      <div class="form-input-group">
-        <label for="email">Email</label>
-        <input type="text" id="email" v-model="user.email" required autofocus />
-      </div>
-      <div class="form-input-group">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model="user.password" required />
-      </div>
-      <div class="form-input-group">
-        <label for="confirmPassword">Confirm Password</label>
-        <input type="password" id="confirmPassword" v-model="user.confirmPassword" required />
-      </div>
-      <button type="submit">Create Account</button>
-      <p><router-link v-bind:to="{ name: 'login' }">Already have an account? Log in.</router-link></p>
-    </form>
+        <p><router-link v-bind:to="{ name: 'login' }">Already have an account? Log in.</router-link></p>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -62,11 +68,11 @@ export default {
           .register(this.user)
           .then(() => {
 
-              this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
-              });
-            }
+            this.$router.push({
+              path: '/login',
+              query: { registration: 'success' },
+            });
+          }
           )
           .catch((error) => {
             const response = error.response;
@@ -91,15 +97,55 @@ export default {
 <style scoped>
 .form-input-group {
   margin-bottom: 1rem;
+
 }
-label {
-  margin-right: 0.5rem;
+
+/* label {
+  /* margin-right: 0.5rem; 
+}*/
+
+
+#container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 50px;
+
+}
+
+.text-center {
+  display: flex;
+  width: 350px;
+  height: 500px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 2rem;
+  border-width: 2px;
+  margin-top: 20px;
+  /* background-color: #F9F6F0; */
+  border-style: outset;
+  border-color: #F4ECE1;
+  margin-bottom: 20px;
+  background: rgba(244, 236, 225, .5);
+
 }
 
 div[role=alert] {
-color: red;
-font-weight: 500;
+  color: red;
+  font-weight: 500;
 
+}
+
+h1 {
+  font-size: 1.5rem;
+}
+
+a {
+  font-weight: 750;
+  color: #314668;
 }
 
 </style>
