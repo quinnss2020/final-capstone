7957ead42c8b608509e851ae61abf1c5125b641e
Quinn Santucci <quinn.s.santucci@gmail.com>
2024-04-17 09:18:40 -0400

    Photos in Unit Details

--- vue/src/components/Photos.vue
+++ vue/src/components/Photos.vue
@@ -1,12 +1,4 @@
 <template>
-  <v-container>
-    <v-carousel :show-arrows="false" >
-      <v-carousel-item  id="image" v-for="(photo,id) in photos" :key="id"
-        :src="photo.url"
-        
-      ></v-carousel-item>
-    </v-carousel>
-  </v-container>
 </template>
 
 <script>
@@ -22,28 +14,9 @@ export default {
   },
 
   methods: {
-    getPhotoList() {
-      PhotoService
-      .getPhotos(this.unitId)
-        .then((response) => {
-          this.photos = response.data;
-        })
-        .catch((error) => {
-          if (error.response) {
-            console.log("Error loading photos: ", error.response.status);
-          } else if (error.request) {
-            console.log(
-              "Error loading photos. Unable to communicate to server."
-            );
-          } else {
-            console.log("Error making request");
-          }
-        });
-    },
   },
 
   created() {
-    this.getPhotoList();
     this.photo1 = this.photos.at(0);
   }
 };
