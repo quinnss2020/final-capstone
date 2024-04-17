f4a7012836b804d97622472cee005452bfccb0a0
Tiana Silva <tianarenees@gmail.com>
2024-04-17 11:15:06 -0400

    ListUnit has a photo in the PhotoCard

--- vue/src/components/UnitCard.vue
+++ vue/src/components/UnitCard.vue
@@ -1,20 +1,27 @@
 <template>
     <section class="unit-card">
         <div id="writing-box">
+            <div class="pic">
+                <img id="card-photo" :src="firstPhoto.url" alt="storage unit for auction" />
+            </div>
             <h3 id="top-line">{{ item.city }} #{{ item.id }} | {{ item.size }}</h3>
             <br>
-            <p id="closing-time">Closes in: <Countdown :expiration="item.expiration"></Countdown></p>
+            <p id="closing-time">Closes in: <Countdown :expiration="item.expiration"></Countdown>
+            </p>
             <br>
             <p id="highest-bid">High bid: ${{ item.highestBid }}</p>
             <br>
-            <v-btn elevation="8" rounded="xl" size="regular" :ripple="true"  v-if="this.$store.state.user.id === 1" v-on:click="this.$router.push({ name: 'adminDetailsView', params: { unitId: item.id }})">EDIT</v-btn>
-            <v-btn elevation="8" rounded="xl" size="regular" :ripple="true"  v-else v-on:click="this.$router.push({ name: 'unitDetails', params: { unitId: item.id }})">BID NOW</v-btn>
+            <v-btn elevation="8" rounded="xl" size="regular" :ripple="true" v-if="this.$store.state.user.id === 1"
+                v-on:click="this.$router.push({ name: 'adminDetailsView', params: { unitId: item.id } })">EDIT</v-btn>
+            <v-btn elevation="8" rounded="xl" size="regular" :ripple="true" v-else
+                v-on:click="this.$router.push({ name: 'unitDetails', params: { unitId: item.id } })">BID NOW</v-btn>
         </div>
     </section>
 </template>
 
 <script>
 import Countdown from '../components/Countdown.vue';
+import PhotoService from '../services/PhotoService.js';
 
 export default {
     name: 'unit',
@@ -23,24 +30,44 @@ export default {
 
     data() {
         return {
-          expiring: "",
-            
+            expiring: "",
+            photos: [],
+            firstPhoto: {},
+
         }
     },
 
     created() {
         setInterval(this.countdown, 1000);
 
+
+        PhotoService
+            .getPhotos(this.item.id)
+            .then((response) => {
+                this.photos = response.data;
+                this.firstPhoto = this.photos[1];
+            })
+            .catch((error) => {
+                if (error.response) {
+                    console.log("Error loading photos: ", error.response.status);
+                }
+                else if (error.request) {
+                    console.log("Error loading photos. Unable to communicate to server.");
+                }
+                else {
+                    console.log("Error making request for unit photos");
+                }
+            })
     }
 
-
 }
+
 
 </script>
 
 <style scoped>
 .unit-card {
-   
+
     display: flex;
     background: rgba(235, 229, 220, 0.5);
     border-color: #faefe0;
@@ -55,8 +82,13 @@ export default {
 
 }
 
-#writing-box{
-    
+#card-photo {
+    height: 150px;
+    width: 200px;
+}
+
+#writing-box {
+
     /* flex-wrap:wrap; */
     /* flex-direction: column;
     justify-content: center;
@@ -65,13 +97,15 @@ export default {
     height: 200px;
 }
 
-p{
+p {
     font-weight: 700;
 }
-h3{
+
+h3 {
     font-size: 1.2rem;
 }
-#closing-time{
+
+#closing-time {
     font-size: 18px;
     font-weight: 800;
     color: #6a3212a7;
